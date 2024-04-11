018427179f6ccefd20412ae894502bdfa1e2c7f2
Vinicius Rodrigues <vinicius.rodrigues2004@gmail.com>
2024-04-10 21:39:34 -0300

    ✨ feat: Adicionando exibição dos dados do grupo.

--- desafio-array.dart
+++ desafio-array.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 void main(List<String> args) {
+  const int escolhaSair = 5;
   final pacientes = [
     'Rodrigo Rahman|35|Desenvolvedor|SP',
     'Manoel Silva|12|Estudante|AL',
@@ -17,9 +18,9 @@ void main(List<String> args) {
 
   int escolha = -1;
 
-  while (escolha != 4) {
+  while (escolha != escolhaSair) {
     print(
-        "Escolha o numero da opção desejada:\n1-Ver dados dos pacientes\n2-Cadastrar paciente\n3-Excluir paciente\n4-Sair");
+        "Escolha o numero da opção desejada:\n1-Ver dados dos pacientes\n2-Ver dados dos membros do grupo.\n3-Cadastrar paciente\n4-Excluir paciente\n${escolhaSair}-Sair");
     var input = stdin.readLineSync();
     if (input != null) {
       escolha = int.parse(input);
@@ -28,12 +29,15 @@ void main(List<String> args) {
           dadosLista(pacientes);
           break;
         case 2:
-          cadastraPaciente(pacientes);
+          dadosGrupo(pacientes);
           break;
         case 3:
-          excluiPaciente(pacientes);
+          cadastraPaciente(pacientes);
           break;
         case 4:
+          excluiPaciente(pacientes);
+          break;
+        case escolhaSair:
           print("Saindo do sistema.");
           break;
         default:
@@ -70,43 +74,66 @@ void dadosLista(List<String> pacientes) {
             (pacienteEstado[dadosPaciente[3]]! + 1);
   }
   print("----------------------//-------------------");
+  // print do numero de pacientes na lista de pacientes
+  print("Numero de pacientes na lista: ${pacientes.length}\n");
   print("Pacientes com mais de 20 anos: ${maiores.join(', ')}\n");
   print("Numero de pacientes por profissão: ${pacienteProfissao}\n");
   print("Numero de pacientes que moram em SP: ${pacienteEstado['SP']}\n");
   print("----------------------//-------------------");
+  print("PRESSIONE ENTER PARA VOLTAR AO MENU!");
+  stdin.readLineSync();
+}
+
+void dadosGrupo(List<String> pacientes) {
+  List<String> membrosGrupo = [
+    "Lucas Caetano",
+    "Vinicius Rodrigues",
+    "Kaua Vieira"
+  ];
+  print("----------------------//-------------------");
+  print("Membros do grupo:");
+  for (String paciente in pacientes) {
+    String nomePaciente = paciente.split("|")[0];
+    if (membrosGrupo.contains(nomePaciente)) {
+      print(paciente);
+    }
+  }
+
+  print("----------------------//-------------------\n");
+  print("PRESSIONE ENTER PARA VOLTAR AO MENU!");
+  stdin.readLineSync();
 }
 
 List<String> cadastraPaciente(List<String> pacientes) {
-
   print("\nInforme o nome do paciente: ");
   var nome = stdin.readLineSync();
-  nome = nome == "" ? "Sem Nome": nome; 
+  nome = nome == "" ? "Sem Nome" : nome;
 
   print("\nInforme a idade do paciente: ");
   var idade = stdin.readLineSync();
-    idade = idade == "" ? "0": idade; 
+  idade = idade == "" ? "0" : idade;
 
   print("\nInforme a profissão do paciente: ");
   var profissao = stdin.readLineSync();
-    profissao = profissao == "" ? "Sem Profissão": profissao; 
+  profissao = profissao == "" ? "Sem Profissão" : profissao;
 
   print("\nInforme a UF do paciente: ");
   var UF = stdin.readLineSync();
-    UF = UF == "" ? "Sem UF": UF; 
+  UF = UF == "" ? "Sem UF" : UF;
 
   pacientes.add("${nome}|${idade}|${profissao}|${UF}");
 
   return pacientes;
-
 }
 
 // função para exclusão de paciente
 List<String> excluiPaciente(List<String> pacientes) {
   print("Informe o nome do paciente que deseja excluir: ");
   var nome = stdin.readLineSync();
-  nome = nome == "" ? "Sem Nome": nome; 
+  nome = nome == "" ? "Sem Nome" : nome;
 
-  pacientes.removeWhere((paciente) => paciente.contains(nome as Pattern));
+  pacientes.removeWhere(
+      (paciente) => paciente.toLowerCase().contains(nome!.toLowerCase()));
 
   return pacientes;
 }
